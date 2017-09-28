540a743470eeb4fe11df4feb22bda691c349c851
林锐 <rui.lin@enai.im>
2017-09-28 09:46:11 +0800

    add unsorted-array-remove function

---
+++ utils/unsorted-array-remove.js
@@ -0,0 +1,21 @@
+/*
+usage:
+let list = ['a', 'b', 'c', 'd', 'e']
+remove(list, 2) // remove 'c'
+console.log(list) // returns ['a', 'b', 'e', 'd'] (no 'c')
+
+This works by popping the last element 
+(which is fast because it doesn't need shift all array elements) 
+and overwriting the removed index with this element.
+ */
+
+export function remove (arr, i) {
+  if (i >= arr.length || i < 0) return
+  var last = arr.pop()
+  if (i < arr.length) {
+    var tmp = arr[i]
+    arr[i] = last
+    return tmp
+  }
+  return last
+}
