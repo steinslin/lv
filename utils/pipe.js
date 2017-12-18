98e491e4240c7ebfe3dfed3f02a523584641f759
linrui <linrui@gridsum.com>
2017-12-18 15:47:17 +0800

    add pipe

---
+++ utils/pipe.js
@@ -0,0 +1,8 @@
+const pipeline = (...funcs) =>
+  val => funcs.reduce((a, b) => b(a), val);
+
+const plus1 = a => a + 1;
+const mult2 = a => a * 2;
+const addThenMult = pipeline(plus1, mult2);
+
+addThenMult(5)
