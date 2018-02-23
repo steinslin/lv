7659b3c6c9de63e72ba4c1a68f0373fe3bed86e4
linrui <linrui@gridsum.com>
2018-02-23 19:49:02 +0800

    update promisify

--- utils/promisify.js
+++ utils/promisify.js
@@ -1,4 +1,10 @@
 export function promisify (fn) {
+  if (typeof fn !== 'function') {
+    throw new TypeError('Argument to promisify must be a function')
+  }
+  if (fn.then) {
+    return fn
+  }
   return (...args) => {
     return new Promise((resolve, reject) => {
       fn.call(this, ...args, (err, ..._args) => {
