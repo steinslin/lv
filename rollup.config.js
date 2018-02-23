2bf33dbb69cb2a71488d74b090790012c6f95779
linrui <linrui@gridsum.com>
2018-02-23 19:42:23 +0800

    add rollup bundle

---
+++ rollup.config.js
@@ -0,0 +1,21 @@
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import babel from 'rollup-plugin-babel'
+
+export default {
+  input: 'index.js',
+  output: [{
+    file: 'lib/index.esm.js',
+    format: 'es'
+  }, {
+    file: 'lib/index.common.js',
+    format: 'cjs'
+  }],
+  plugins: [resolve(), commonjs(), babel({
+    presets: [['env', {
+      'modules': false
+    }], 'stage-2'],
+    runtimeHelpers: true,
+    babelrc: false
+  })]
+}
