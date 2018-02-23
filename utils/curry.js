e12b4af747bf9ae9d36abe0b53b0819e5077d162
linrui <linrui@gridsum.com>
2018-02-23 19:43:41 +0800

    modified curry

--- utils/curry.js
+++ utils/curry.js
@@ -2,13 +2,13 @@
 **  使用递归，对一个函数进行curry，第2个参数可以设置等待的参数个数，默认为传递的函数参数个数
 */
 
-export const curry = (f, arity = f.length) => {
-  return (...args) =>
-    args.length >= arity ? f(...args) : (...nextArgs) => curry(f, arity)(...args, ...nextArgs)
-}
+// export const curry = (f, arity = f.length) => {
+//   return (...args) =>
+//     args.length >= arity ? f(...args) : (...nextArgs) => curry(f, arity)(...args, ...nextArgs)
+// }
 
 /*
 **  更精炼
 */
-export const currying = (f, arity = f.length, ...args) =>
-  args.length >= arity ? f(...args) : currying.bind(null, f, arity, ...args)
+export const curry = (f, arity = f.length, ...args) =>
+  args.length >= arity ? f(...args) : curry.bind(null, f, arity, ...args)
