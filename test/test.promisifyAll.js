1fd796058ac659a1958ba96632cdc977e18a66d7
linrui <linrui@gridsum.com>
2018-03-23 22:14:40 +0800

    add test

--- test/test.promisifyAll.js
+++
@@ -1,4 +0,0 @@
-const {promisifyAll} = require('../lib/index.common.js')
-const fs = promisifyAll(require('fs'))
-
-console.log(fs.readFileAsync)
---
+++ test/utils.spec.js
@@ -0,0 +1,30 @@
+import { expect } from 'chai'
+import { promisify, promisifyAll } from '../lib/index.esm.js'
+import { describe, it } from 'mocha'
+
+function read (name, cb) {
+  setTimeout(() => {
+    cb(null, name)
+  }, 30)
+}
+
+function read2 (name, cb) {
+  setTimeout(() => {
+    cb(null, name)
+  }, 30)
+}
+
+describe('utils', () => {
+  it('promisify', async () => {
+    const readAsync = promisify(read)
+    const result = await readAsync('lin')
+    expect(result).to.equal('lin')
+  })
+  it('promisifyAll', async () => {
+    const obj = promisifyAll({ read, read2 })
+    const result = await obj.readAsync('lin')
+    const result2 = await obj.read2Async('rui')
+    expect(result).to.equal('lin')
+    expect(result2).to.equal('rui')
+  })
+})
